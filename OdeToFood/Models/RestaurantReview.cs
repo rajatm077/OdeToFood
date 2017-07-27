fb61c151e9bf524ed16990ffa016c9b196c276b7
MalhotR1 <rajatmalhotra26@gmail.com>
2017-07-27 14:41:05 +0530

    updated js libraries.

--- OdeToFood/Models/RestaurantReview.cs
+++ OdeToFood/Models/RestaurantReview.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Web;
 
 namespace OdeToFood.Models {
+    
     public class RestaurantReview {
         public int Id { get; set; }
 
@@ -12,9 +13,12 @@ namespace OdeToFood.Models {
         [StringLength(100)]
         public string Body { get; set; }
         public int RestaurantId { get; set; }
+
+        [Display(Name = "Reviewer Name")]
+        [DisplayFormat(NullDisplayText = "anonymous")]
         public string ReviewerName { get; set; }
 
-        [Range(1,10)]
+        [Range(1, 10)]
         public int Rating { get; set; }
     }
 }
