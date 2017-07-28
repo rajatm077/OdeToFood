9686dbf55972cc9b14d79f0d432f913393f44f2e
MalhotR1 <rajatmalhotra26@gmail.com>
2017-07-28 12:14:50 +0530

    Added single bundle file of scripts.

--- OdeToFood/App_Start/BundleConfig.cs
+++ OdeToFood/App_Start/BundleConfig.cs
@@ -5,6 +5,12 @@ namespace OdeToFood {
     public class BundleConfig {
         // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles) {
+            bundles.Add(new ScriptBundle("~/bundles/otf").Include(
+                       "~/Scripts/jquery-{version}.js",
+                       "~/Scripts/jquery-ui-{version}.js",
+                       "~/Scripts/jquery-unobtrusive-{version}.js",
+                       "~/Scripts/jquery.validate*"));
+
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js"));
 
